a8455b08bdd28872fe661b86d58a66258a6ecbe5
CarolGitonga <carolgitongaofficial@gmail.com>
2023-09-18 22:15:27 +0300

    add secret configurations in .env file

--- flomart/settings.py
+++ flomart/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1rg@g*a&s@f=ki(sz@iv&7ql-f0y5p@-kikd-_+z7h60vq0ry-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
