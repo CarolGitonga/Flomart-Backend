8377b5ccee19db4d0db11e22aba3a5ef90bc13ce
CarolGitonga <carolgitongaofficial@gmail.com>
2023-09-20 21:32:46 +0300

    add uuidnto the product and category

--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+import uuid
 from cloudinary.models import CloudinaryField
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Category(models.Model):
-    id = models.AutoField(primary_key=True)
+    id=models.UUIDField(default=uuid.uuid4,unique=True, primary_key=True, editable=False)
     category = models.CharField(max_length=100, blank=True, null=True)
     
 
@@ -12,11 +13,11 @@ class Category(models.Model):
     def __str__(self):
         return self.category
 
-class Tag(models.Model):
-    name = models.CharField(max_length=200)
+#class Tag(models.Model):
+    #name = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.name
+    #def __str__(self):
+        #return self.name
     
 class Product(models.Model):
     title = models.CharField(max_length=200)
@@ -24,11 +25,13 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = CloudinaryField('image', blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product_category', null=True, blank=True)
-    tags = models.ManyToManyField(Tag)
+    #tags = models.ManyToManyField(Tag)
     color = models.CharField(max_length=50)
-    stock_quantity = models.PositiveIntegerField()
+    #stock_quantity = models.PositiveIntegerField()
     date_added = models.DateField()
     featured = models.BooleanField(default=False)
+    id=models.UUIDField(default=uuid.uuid4,unique=True, primary_key=True, editable=False)
+
 
     def __str__(self):
         return self.title
@@ -39,7 +42,7 @@ class Review(models.Model):
     rating = models.IntegerField(default=0, validators=[MaxValueValidator(5),MinValueValidator(1)])
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='post_comment')
     purchased = models.BooleanField(default=False)
-    date = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.product.title
