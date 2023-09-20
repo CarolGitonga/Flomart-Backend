af2f63184c2820a2d84fb7af9590f3f25047cd9c
CarolGitonga <carolgitongaofficial@gmail.com>
2023-09-20 13:42:44 +0300

    create url paths

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path, include
 from . import views
-from django.conf import settings
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
+router = routers.DefaultRouter() #parent router
+
+router.register('products', views.ProductViewSet)
+router.register('categories', views.CategoryViewSet)
+#router.register('tags', views.TagViewSet)
+
+
+product_router = routers.NestedDefaultRouter(router, 'products',lookup='product')#child router)
+product_router.register('reviews', views.ReviewViewSet, basename='product_reviews')
 urlpatterns=[
     path('', views.index, name='index'),
+    path('', include(router.urls)),
+    path('', include(product_router.urls))
 ]
