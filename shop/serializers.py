55b4b590d935d0d49f60d569eaaaa5f23436798d
CarolGitonga <carolgitongaofficial@gmail.com>
2023-09-21 19:24:17 +0300

    serializer relationships

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,20 +1,20 @@
 from rest_framework import serializers
 from shop.models import Category, Product, Review
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['id','title','description','category','image','price','color','date_added','featured']
-
-#class TagSerializer(serializers.ModelSerializer):
-    #class Meta:
-       # model = Tag
-       # fields = ['name']
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id','title']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','description','category','image','price','color','date_added','featured']
+
+    category = CategorySerializer()
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,9 +25,6 @@ class ReviewSerializer(serializers.ModelSerializer):
         product_id = self.context['product_id']
         return Review.objects.create(product_id, **validated_data)
     
-    class SimpleProductSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Product
-            fields = ['id', 'title', 'price']
+    
         
 
