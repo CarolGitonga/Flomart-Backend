199e084d2093f1a3750c884315c7a7178f55b58b
CarolGitonga <carolgitongaofficial@gmail.com>
2023-09-25 16:47:46 +0300

    add slug field to product and category model classe

--- shop/models.py
+++ shop/models.py
@@ -7,6 +7,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Category(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True)
     title = models.CharField(max_length=100, blank=True, null=True)
+    slug = models.SlugField(max_length=200)
     
 
 
@@ -24,14 +25,15 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = CloudinaryField('image', blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product_category', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name='products')
+    #category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product_category', null=True, blank=True)
     #tags = models.ManyToManyField(Tag)
     color = models.CharField(max_length=50)
     #stock_quantity = models.PositiveIntegerField()
     date_added = models.DateField()
     featured = models.BooleanField(default=False)
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True)
-
+    slug = models.SlugField(max_length=200)
 
 
     def __str__(self):
