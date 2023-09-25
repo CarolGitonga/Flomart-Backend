acdbe9da47b5f4c74e1073c41ddb126d5484d867
CarolGitonga <carolgitongaofficial@gmail.com>
2023-09-25 17:03:38 +0300

    add cart class to models

--- shop/models.py
+++ shop/models.py
@@ -49,4 +49,10 @@ class Review(models.Model):
     def __str__(self):
         return self.product.description
 
+class Cart(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
     
