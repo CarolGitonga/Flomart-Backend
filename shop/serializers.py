3f60f3eccc56fe6e4d0022c7387a6f7785fa45fe
CarolGitonga <carolgitongaofficial@gmail.com>
2023-09-25 11:18:45 +0300

    set the category to read only on product serializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -10,22 +10,33 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)# to prevent unnecessary write operations
     class Meta:
         model = Product
         fields = ['id','title','description','category','image','price','color','date_added','featured']
 
-    category = CategorySerializer()
+    
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
     class Meta:
         model = Review
         fields = ['id','name','description','date_added','product']
 
+    #def create(self, validated_data):
+       # product_id = self.context["product_id"]
+        #return Review.objects.create(product_id = product_id,  **validated_data)
+    
+    #def create(self, validated_data):
+        #product_data = validated_data.pop('product')
+        #product_id = self.context.get('product_id')
+        #review = Review.objects.create(product_id=product_id, **validated_data)
+        #return review
     def create(self, validated_data):
         product_id = self.context["product_id"]
-        return Review.objects.create(product_id = product_id,  **validated_data)
-    
-    
+        validated_data.pop('product', None)  # Remove 'product' from validated_data
+        return Review.objects.create(product_id=product_id, **validated_data)
         
 
