24587f4eb0c95041a6b2a5e094882f04b86af90b
CarolGitonga <carolgitongaofficial@gmail.com>
2023-09-25 16:49:20 +0300

    add slug to product and category prepopulated fields

--- shop/admin.py
+++ shop/admin.py
@@ -2,18 +2,17 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    #specifies which fields should be displayed in the admin list view for each model.
-    list_display = ['title','price','date_added']
-    # adds filters in the right sidebar to filter records by specific fields
-    list_filter = ['date_added']
-    #
-    search_fields = ['name','description']
 
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Category, CategoryAdmin)
 #admin.site.register(Tag)
 admin.site.register(Review)
+admin.site.register(Product, ProductAdmin)
 
 
 
