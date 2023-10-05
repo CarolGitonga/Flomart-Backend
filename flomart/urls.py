07c5327f76dbc22a9faccc1f95b4383d3d2c5e09
CarolGitonga <carolgitongaofficial@gmail.com>
2023-10-05 12:43:22 +0300

    add auth app

--- flomart/urls.py
+++ flomart/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
+    path('shop/', include('shop.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 
