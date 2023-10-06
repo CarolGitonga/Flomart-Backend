9baa406bbc66ea4d31034d9db7d8aa18d70edd71
CarolGitonga <carolgitongaofficial@gmail.com>
2023-10-06 15:44:16 +0300

    register user to the admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
 
 # Register your models here.
 
@@ -9,11 +11,20 @@ class ProductAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username","email","password1","password2"),
+        }),
+    )
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Review)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Cart)
 admin.site.register(Cartitems)
+admin.site.register(get_user_model(),UserAdmin)
 
 
 
