1d3a4ec8d21b65aaec0c8b887c9150b5af5fc0e1
CarolGitonga <carolgitongaofficial@gmail.com>
2023-10-06 16:35:41 +0300

    customize user create using djoser

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 
 # Register your models here.
@@ -11,20 +11,24 @@ class ProductAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
-class UserAdmin(BaseUserAdmin):
+class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
             "fields": ("username","email","password1","password2"),
         }),
     )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Review)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Cart)
 admin.site.register(Cartitems)
-admin.site.register(get_user_model(),UserAdmin)
+admin.site.register(get_user_model(),CustomUserAdmin)
 
 
 
